15355947b0f090f25430b94a129e03173ca79832
breinda <brendac@live.it>
2018-09-09 21:18:35 -0300

    first commit

---
+++ get-nasa.js
@@ -0,0 +1,26 @@
+/*
+GOT
+- lightweight!
+- works with Promises
+*/
+
+/*
+the following code:
+- sends a GET request to NASA’s API
+- prints out the URL for the astronomy picture of the day + an explanation
+*/
+
+const got = require ('got');
+
+got ('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true })
+.then (response => {
+  console.log (response.body.url);
+  console.log (response.body.explanation);
+
+})
+.catch (error => {
+  console.log (error.response.body);
+
+});
+
+// console.log('we done wit dis');
---
+++ get-tatooine.js
@@ -0,0 +1,34 @@
+/*
+  gets some data about tatooine
+
+  usage: node get-tatooine.js [-w]
+  -w is used to wookiee-fy the response, and is always the superior option
+*/
+
+const request = require ('request');
+
+var wookiee = ""
+var url = 'https://swapi.co/api/planets/1/'
+
+if (process.argv[2] == "-w") {
+  wookiee = '?format=wookiee'
+  url = url + wookiee
+}
+
+// callback
+request (url, { json: true }, (err, res, body) => {
+  if (err) {
+    return console.log (err);
+  }
+
+  if (wookiee) {
+    console.log (body);
+  }
+  else {
+    console.log (body.name);
+    console.log (body.terrain);
+    console.log (body.residents);
+  }
+});
+
+// console.log('we done wit dis');
